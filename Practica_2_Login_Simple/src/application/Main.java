7fc3a4b0d9a6cf691c1e5f024b28ab5336af1f7f
21752434 <21752434@N203-09.alumnos.uem.es>
2018-11-02 14:05:39 +0100

    Version básica

--- Practica_2_Login_Simple/src/application/Main.java
+++ Practica_2_Login_Simple/src/application/Main.java
@@ -1,23 +1,24 @@
 package application;
 	
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-
+import javafx.scene.layout.AnchorPane;
 
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+	public void start(Stage stage) throws IOException {
+		
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("Login.fxml"));
+		AnchorPane mypane = loader.load();
+		
+		// Añade la escena al stage y la titula
+		stage.setTitle("Login");
+		stage.setScene(new Scene(mypane));
+		stage.show();
 	}
 	
 	public static void main(String[] args) {
